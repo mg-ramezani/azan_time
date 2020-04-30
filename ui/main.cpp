00126f678330c95ecfd0ffd7e1424e48bf97ccfa
Ghasem Ramezani <g1999ramezani@gmail.com>
2020-04-30 11:32:37 +0430

    add min command line argument to minimize windows on startup

--- ui/main.cpp
+++ ui/main.cpp
@@ -5,9 +5,16 @@
 int main(int argc, char* argv[])
 {
     QApplication application(argc, argv);
-
     azan_widget window;
-    window.show();
+
+    if (argc > 1 && std::string(argv[1]) == "min")
+    {
+        window.hide();
+    }
+    else
+    {
+        window.show();
+    }
 
     return application.exec();
 }
